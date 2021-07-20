328af7458f02513840596d7f07478ad9693de67b
diegolins10 <diegofernandeslins@gmail.com>
2021-07-20 20:14:39 -0300

    class exceptions

---
+++ BankAccount/Entities/Exceptions/AccountExceptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BankAccount.Entities.Exceptions
+{
+    /*
+     * Sub classe chamando a super classe ApplicationExceptions
+     * para a criacao de exceptions personalizadas
+     */
+    class AccountExceptions : ApplicationException
+    {
+        public AccountExceptions(string message) : base(message)
+        {
+        }
+    }
+}
