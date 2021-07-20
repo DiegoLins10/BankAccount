a884e89180348c53e79a4c74d81612e9396d827f
diegolins10 <diegofernandeslins@gmail.com>
2021-07-20 20:14:52 -0300

    program try catch

--- BankAccount/Program.cs
+++ BankAccount/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BankAccount.Entities;
+using BankAccount.Entities.Exceptions;
+using System;
 
 namespace BankAccount
 {
@@ -6,7 +8,46 @@ namespace BankAccount
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            try
+            {
+                Console.WriteLine("Enter account data");
+                Console.Write("Number: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Holder: ");
+                string name = Console.ReadLine();
+                Console.Write("Initial Balance: ");
+                double balance = double.Parse(Console.ReadLine());
+                Console.Write("Withdraw limit: ");
+                double limitBalance = double.Parse(Console.ReadLine());
+                Account account = new Account(number, name, balance, limitBalance);
+
+                Console.WriteLine();
+                Console.Write("Enter amount for withdraw: ");
+                double withdraw = double.Parse(Console.ReadLine());
+                account.WithDraw(withdraw);
+                Console.WriteLine(account);
+            }
+            /*
+             * Exception personalizada
+             */
+            catch (AccountExceptions e)
+            {
+                Console.WriteLine("Withdraw error: " + e.Message);
+            }/*
+              * exception de formato de entrada
+              */
+            catch(FormatException e)
+            {
+                Console.WriteLine("Format error: " + e.Message);
+            }
+            /*
+             * exception Generica
+             */
+            catch (Exception e)
+            {
+                Console.WriteLine("Unexpected error: " + e.Message);
+            }
         }
     }
 }
